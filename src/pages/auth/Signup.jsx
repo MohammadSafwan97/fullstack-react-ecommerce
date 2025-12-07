3b701a40b465130da650d8dd3fff4187d34a6505
Muhammad Safwan <mohammadsafwan248@gmail.com>
2025-12-07 11:39:10 +0500

    signup text change

--- src/pages/auth/Signup.jsx
+++ src/pages/auth/Signup.jsx
@@ -103,7 +103,7 @@ const Signup = () => {
             className="text-white bg-indigo-500 border-0 py-2 px-6 
             focus:outline-none hover:bg-indigo-600 rounded text-lg"
           >
-            Sign Up
+            CREATE YOUR ACCOUNT
           </button>
 
           {/* Already have account */}
